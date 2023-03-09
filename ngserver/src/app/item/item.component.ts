b086653dbd93cdf1022dba24219ae775801f24fd
Manuel <90761259+ManuelParma@users.noreply.github.com>
2023-03-09 14:51:51 +0100

    Punto 8

--- ngserver/src/app/item/item.component.ts
+++ ngserver/src/app/item/item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OpenffService } from '../openff.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-item',
@@ -12,7 +13,7 @@ export class ItemComponent implements OnInit {
   res: any = {};
   id: any = 0;
 
-  constructor(private off: OpenffService, private route: ActivatedRoute) { }
+  constructor(private off: OpenffService, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit(): void {
     // this.id = this.route.snapshot.paramMap.get("id");
@@ -36,5 +37,9 @@ export class ItemComponent implements OnInit {
   getIng(l: any) {
     if (!l) return;
     return l.map((e: any) => e.id.replace(/[A-z]*\:/, ""))
+  }
+
+  goBack() {
+    this.location.back();
   }
 }
